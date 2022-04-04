8c8bb7f8605c4207f44ebbcb907557177cdba059
lazaronazareno <lazaronazareno@gmail.com>
2022-04-04 08:40:41 -0300

    header styles changes in new file

--- kanokano/src/Components/Header/Header.js
+++ kanokano/src/Components/Header/Header.js
@@ -4,13 +4,14 @@ import { Link } from 'react-router-dom';
 import logo from '../../Assets/logo@2x.png';
 import '../../Colors/theme.scss';
 import Menu from '../MainMenu/Menu';
+import './styles.scss';
 
 function Header() {
     const theme = useSelector(store => store.kanokano.theme)
     return (
         <header className={`container-fluid header d-flex position-absolute top-0 ${theme}`}>
             <Link to="/home">
-              <img className="img-fluid" src={logo} alt="img"/>
+              <img className="img-fluid header-img" src={logo} alt="img"/>
             </Link>
             <Menu className="justify-self-end"/>
         </header>
